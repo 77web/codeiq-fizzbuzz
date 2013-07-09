e330e573da7335cb9c4355107124a34e2a2d8c82
77web <info@77-web.com>
2013-07-09 16:01:27 +0900

    問題に示されたメソッドを持つFizzBuzzSpecificationクラスを作成

---
+++ FizzBuzzSpecification.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CodeIQ;
+
+class FizzBuzzSpecification
+{
+    /**
+     * @param int $divisor
+     */
+    public function __construct($divisor)
+    {
+    }
+
+    /**
+     * @param int $dividend
+     * @return bool
+     */
+    public function isSatisfiedBy($dividend)
+    {
+    }
+}

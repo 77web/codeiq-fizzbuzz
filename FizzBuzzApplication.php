75edf9c30ac42dd4cbcb2c9087df3704d66ff77f
77web <info@77-web.com>
2013-07-09 16:02:03 +0900

    問題に示されたメソッドを持つFizzBuzzApplicationクラスを作成

---
+++ FizzBuzzApplication.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CodeIQ;
+
+class FizzBuzzApplication
+{
+    /**
+     * @param FizzBuzzSpecification $spec
+     * @param string $message
+     */
+    public function addSpecAndMessage(FizzBuzzSpecification $spec, $message)
+    {
+    }
+
+    /**
+     * @param array $data
+     */
+    public function run(array $data)
+    {
+    }
+}
